c95f0e03fd9ca0ba6fc1d2c187df01209d8965f4
Rita Doumet <rita.17.2001@gmail.com>
2022-07-24 23:31:56 +0300

    contact-me

--- script.js
+++ script.js
@@ -97,3 +97,16 @@ function expand(){
     }
     expanded=!expanded;
 }
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if(inputtxt.value.match(phoneno))
+     {
+	   return true;
+	 }
+   else
+     {
+	   alert("Not a valid Phone Number");
+	   return false;
+     }
+}
